7502edee975a2f4e36b0d4d1c0eed093bcdf582d
Dylan <Dylan@DESKTOP-MCP1CIF>
2026-06-02 19:47:08 -0500

    Se implementa formulario de gestion de pedidos y calculo automatico de totales

--- Formularios/frmPedidos.Designer.cs
+++ Formularios/frmPedidos.Designer.cs
@@ -47,6 +47,17 @@
             this.label8 = new System.Windows.Forms.Label();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.label9 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // titulo
@@ -61,7 +72,8 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(395, 216);
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(395, 256);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(128, 22);
             this.textBox4.TabIndex = 26;
@@ -69,15 +81,16 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(330, 219);
+            this.label4.Location = new System.Drawing.Point(350, 256);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(41, 16);
+            this.label4.Size = new System.Drawing.Size(31, 16);
             this.label4.TabIndex = 25;
-            this.label4.Text = "Total:";
+            this.label4.Text = "IVA:";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(395, 176);
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(395, 216);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(128, 22);
             this.textBox3.TabIndex = 24;
@@ -85,15 +98,15 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(340, 179);
+            this.label3.Location = new System.Drawing.Point(322, 219);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(31, 16);
+            this.label3.Size = new System.Drawing.Size(59, 16);
             this.label3.TabIndex = 23;
-            this.label3.Text = "IVA:";
+            this.label3.Text = "Subtotal:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(137, 182);
+            this.textBox2.Location = new System.Drawing.Point(395, 174);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(128, 22);
             this.textBox2.TabIndex = 22;
@@ -101,11 +114,11 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(62, 182);
+            this.label2.Location = new System.Drawing.Point(320, 174);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 16);
+            this.label2.Size = new System.Drawing.Size(64, 16);
             this.label2.TabIndex = 21;
-            this.label2.Text = "Subtotal:";
+            this.label2.Text = "Cantidad:";
             // 
             // textBox1
             // 
@@ -127,10 +140,7 @@
             // 
             this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Items.AddRange(new object[] {
-            "True",
-            "False"});
-            this.comboBox2.Location = new System.Drawing.Point(395, 88);
+            this.comboBox2.Location = new System.Drawing.Point(137, 174);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(128, 24);
             this.comboBox2.TabIndex = 30;
@@ -138,7 +148,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(320, 91);
+            this.label7.Location = new System.Drawing.Point(62, 177);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(56, 16);
             this.label7.TabIndex = 29;
@@ -148,12 +158,6 @@
             // 
             this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Plato Fuerte",
-            "Sopa",
-            "Bebida",
-            "Postre",
-            "Desayuno"});
             this.comboBox1.Location = new System.Drawing.Point(137, 130);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(128, 24);
@@ -172,10 +176,7 @@
             // 
             this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Items.AddRange(new object[] {
-            "True",
-            "False"});
-            this.comboBox3.Location = new System.Drawing.Point(137, 226);
+            this.comboBox3.Location = new System.Drawing.Point(137, 214);
             this.comboBox3.Name = "comboBox3";
             this.comboBox3.Size = new System.Drawing.Size(128, 24);
             this.comboBox3.TabIndex = 34;
@@ -183,23 +184,17 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(65, 226);
+            this.label5.Location = new System.Drawing.Point(65, 214);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(53, 16);
+            this.label5.Size = new System.Drawing.Size(44, 16);
             this.label5.TabIndex = 33;
-            this.label5.Text = "Estado:";
+            this.label5.Text = "Mesa:";
             // 
             // comboBox4
             // 
             this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox4.FormattingEnabled = true;
-            this.comboBox4.Items.AddRange(new object[] {
-            "Plato Fuerte",
-            "Sopa",
-            "Bebida",
-            "Postre",
-            "Desayuno"});
-            this.comboBox4.Location = new System.Drawing.Point(395, 131);
+            this.comboBox4.Location = new System.Drawing.Point(395, 88);
             this.comboBox4.Name = "comboBox4";
             this.comboBox4.Size = new System.Drawing.Size(128, 24);
             this.comboBox4.TabIndex = 32;
@@ -207,16 +202,16 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(332, 134);
+            this.label8.Location = new System.Drawing.Point(340, 88);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(44, 16);
+            this.label8.Size = new System.Drawing.Size(41, 16);
             this.label8.TabIndex = 31;
-            this.label8.Text = "Mesa:";
+            this.label8.Text = "Plato:";
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(137, 271);
+            this.dateTimePicker1.Location = new System.Drawing.Point(137, 254);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(120, 22);
@@ -225,17 +220,131 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(72, 271);
+            this.label9.Location = new System.Drawing.Point(72, 254);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(48, 16);
             this.label9.TabIndex = 35;
             this.label9.Text = "Fecha:";
             // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(395, 294);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(128, 22);
+            this.textBox5.TabIndex = 38;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(340, 294);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 16);
+            this.label10.TabIndex = 37;
+            this.label10.Text = "Total:";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Items.AddRange(new object[] {
+            "Pendiente",
+            "Preparando",
+            "Entregado",
+            "Cancelado"});
+            this.comboBox5.Location = new System.Drawing.Point(395, 130);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(128, 24);
+            this.comboBox5.TabIndex = 40;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(332, 133);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(53, 16);
+            this.label11.TabIndex = 39;
+            this.label11.Text = "Estado:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(255, 354);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 35);
+            this.button3.TabIndex = 43;
+            this.button3.Text = "Eliminar";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(157, 354);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 35);
+            this.button2.TabIndex = 42;
+            this.button2.Text = "Editar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 35);
+            this.button1.TabIndex = 41;
+            this.button1.Text = "Agregar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(355, 354);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 35);
+            this.button4.TabIndex = 44;
+            this.button4.Text = "Calcular";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(457, 354);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 35);
+            this.button5.TabIndex = 45;
+            this.button5.Text = "Limpiar";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(27, 412);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(702, 242);
+            this.dataGridView1.TabIndex = 46;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
             // frmPedidos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(776, 697);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label10);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.comboBox3);
@@ -257,6 +366,8 @@
             this.Controls.Add(this.titulo);
             this.Name = "frmPedidos";
             this.Text = "frmPedidos";
+            this.Load += new System.EventHandler(this.frmPedidos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -283,5 +394,15 @@
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Formularios/frmPedidos.cs
+++ Formularios/frmPedidos.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaRestaurante.Controlador;
+using SistemaRestaurante.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,248 @@ namespace SistemaRestaurante.Formularios
         public frmPedidos()
         {
             InitializeComponent();
+        }
+
+        public void CargarCombos()
+        {
+            comboBox1.DataSource = null;
+            comboBox1.DataSource = TListaCliente.Lista;
+            comboBox1.DisplayMember = "Nombre";
+
+            comboBox2.DataSource = null;
+            comboBox2.DataSource = TLista.ListaMeseros;
+            comboBox2.DisplayMember = "Nombre";
+
+            comboBox3.DataSource = null;
+            comboBox3.DataSource = TLista.ListaMesas;
+            comboBox3.DisplayMember = "Numero";
+
+            comboBox4.DataSource = null;
+            comboBox4.DataSource = TListaPlato.Lista;
+            comboBox4.DisplayMember = "Nombre";
+        }
+
+        public void Listar()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = TListaPedido.Lista.ToList();
+        }
+
+        public void Calcular()
+        {
+            Plato plato = (Plato)comboBox4.SelectedItem;
+
+            int cantidad = int.Parse(textBox2.Text);
+
+            decimal subtotal = plato.Precio * cantidad;
+
+            decimal iva = subtotal * 0.15m;
+
+            decimal total = subtotal + iva;
+
+            textBox3.Text = subtotal.ToString();
+
+            textBox4.Text = iva.ToString();
+
+            textBox5.Text = total.ToString();
+        }
+
+        public Pedido CrearObjeto()
+        {
+            int idPedido = int.Parse(textBox1.Text);
+
+            Cliente cliente =
+                (Cliente)comboBox1.SelectedItem;
+
+            Mesero mesero =
+                (Mesero)comboBox2.SelectedItem;
+
+            Mesa mesa =
+                (Mesa)comboBox3.SelectedItem;
+
+            Plato plato =
+                (Plato)comboBox4.SelectedItem;
+
+            int cantidad =
+                int.Parse(textBox2.Text);
+
+            decimal subtotal =
+                decimal.Parse(textBox3.Text);
+
+            decimal iva =
+                decimal.Parse(textBox4.Text);
+
+            decimal total =
+                decimal.Parse(textBox5.Text);
+
+            string estado =
+                comboBox5.SelectedItem.ToString();
+
+            DateTime fecha =
+                dateTimePicker1.Value;
+
+            return new Pedido(
+                idPedido,
+                cliente,
+                mesero,
+                mesa,
+                plato,
+                cantidad,
+                subtotal,
+                iva,
+                total,
+                estado,
+                fecha
+            );
+        }
+
+        public bool Validar()
+        {
+            return !textBox1.Text.Equals("") &&
+                   !textBox2.Text.Equals("") &&
+                   !textBox3.Text.Equals("") &&
+                   !textBox4.Text.Equals("") &&
+                   !textBox5.Text.Equals("") &&
+                   comboBox1.SelectedIndex >= 0 &&
+                   comboBox2.SelectedIndex >= 0 &&
+                   comboBox3.SelectedIndex >= 0 &&
+                   comboBox4.SelectedIndex >= 0 &&
+                   comboBox5.SelectedIndex >= 0;
+        }
+
+        public void Limpiar()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+
+            comboBox1.SelectedIndex = -1;
+            comboBox2.SelectedIndex = -1;
+            comboBox3.SelectedIndex = -1;
+            comboBox4.SelectedIndex = -1;
+            comboBox5.SelectedIndex = -1;
+        }
+
+        private void frmPedidos_Load(object sender, EventArgs e)
+        {
+            CargarCombos();
+            Listar();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Validar())
+                {
+                    Pedido p = CrearObjeto();
+
+                    p.Plato.DisminuirStock(p.Cantidad);
+
+                    TListaPedido.Insert(p);
+
+                    Listar();
+
+                    Limpiar();
+                }
+                else
+                {
+                    MessageBox.Show("Complete todos los campos");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.CurrentRow != null)
+                {
+                    int pos = dataGridView1.CurrentRow.Index;
+
+                    TListaPedido.Update(pos, CrearObjeto());
+
+                    Listar();
+
+                    Limpiar();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.CurrentRow != null)
+                {
+                    DialogResult r = MessageBox.Show(
+                        "¿Desea eliminar el pedido?",
+                        "Eliminar",
+                        MessageBoxButtons.YesNo);
+
+                    if (r == DialogResult.Yes)
+                    {
+                        int pos = dataGridView1.CurrentRow.Index;
+
+                        TListaPedido.Delete(pos);
+
+                        Listar();
+
+                        Limpiar();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Calcular();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int pos = e.RowIndex;
+
+            if (pos >= 0)
+            {
+                Pedido p = TListaPedido.GetPedido(pos);
+
+                textBox1.Text = p.IdPedido.ToString();
+
+                comboBox1.SelectedItem = p.Cliente;
+                comboBox2.SelectedItem = p.Mesero;
+                comboBox3.SelectedItem = p.Mesa;
+                comboBox4.SelectedItem = p.Plato;
+
+                textBox2.Text = p.Cantidad.ToString();
+
+                textBox3.Text = p.Subtotal.ToString();
+                textBox4.Text = p.Iva.ToString();
+                textBox5.Text = p.Total.ToString();
+
+                comboBox5.SelectedItem = p.Estado;
+
+                dateTimePicker1.Value = p.FechaHora;
+            }
         }
     }
 }
