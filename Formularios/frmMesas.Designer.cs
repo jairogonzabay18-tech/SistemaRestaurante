f2b145992b7fcc05d9ac084a84e61f20ceac5a08
Dylan <Dylan@DESKTOP-MCP1CIF>
2026-06-02 20:07:37 -0500

    Se implementa formulario de gestion de mesas

--- Formularios/frmMesas.Designer.cs
+++ Formularios/frmMesas.Designer.cs
@@ -28,12 +28,77 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.titulo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.Location = new System.Drawing.Point(291, 46);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(76, 25);
+            this.titulo.TabIndex = 2;
+            this.titulo.Text = "Mesas";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(65, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 34;
+            this.button1.Text = "Reservar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(49, 165);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(702, 242);
+            this.dataGridView1.TabIndex = 33;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(215, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 35;
+            this.button2.Text = "Liberar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // frmMesas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.titulo);
+            this.Name = "frmMesas";
             this.Text = "frmMesas";
+            this.Load += new System.EventHandler(this.frmMesas_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
     }
 }
--- Formularios/frmMesas.cs
+++ Formularios/frmMesas.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaRestaurante.Controlador;
+using SistemaRestaurante.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,67 @@ namespace SistemaRestaurante.Formularios
         public frmMesas()
         {
             InitializeComponent();
+        }
+        public void Listar()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = TLista.ListaMesas.ToList();
+        }
+
+        private void frmMesas_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int pos = dataGridView1.CurrentRow.Index;
+
+                Mesa mesa = TLista.ListaMesas[pos];
+
+                if (mesa.Disponible)
+                {
+                    mesa.Reservar();
+
+                    Listar();
+
+                    MessageBox.Show("Mesa reservada");
+                }
+                else
+                {
+                    MessageBox.Show("La mesa ya está ocupada");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                int pos = dataGridView1.CurrentRow.Index;
+
+                Mesa mesa = TLista.ListaMesas[pos];
+
+                mesa.Liberar();
+
+                Listar();
+
+                MessageBox.Show("Mesa liberada");
+            }
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int pos = e.RowIndex;
+
+            if (pos >= 0)
+            {
+                Mesa mesa = TLista.ListaMesas[pos];
+
+                MessageBox.Show(mesa.ImprimirMesa());
+            }
         }
     }
 }
