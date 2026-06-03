a3c8bac281dff780214cde35f44345255aaf7a99
Dylan <Dylan@DESKTOP-MCP1CIF>
2026-06-02 20:01:54 -0500

    Se implementa formulario de consulta de empleados

--- Formularios/frmEmpleados.Designer.cs
+++ Formularios/frmEmpleados.Designer.cs
@@ -28,12 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.titulo = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titulo
+            // 
+            this.titulo.AutoSize = true;
+            this.titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titulo.Location = new System.Drawing.Point(266, 28);
+            this.titulo.Name = "titulo";
+            this.titulo.Size = new System.Drawing.Size(119, 25);
+            this.titulo.TabIndex = 1;
+            this.titulo.Text = "Empleados";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(234, 84);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(128, 24);
+            this.comboBox1.TabIndex = 30;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(107, 87);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 16);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Tipo Empleado:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(53, 196);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(702, 242);
+            this.dataGridView1.TabIndex = 31;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 139);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 35);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Ver informacion";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmEmpleados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.titulo);
+            this.Name = "frmEmpleados";
             this.Text = "frmEmpleados";
+            this.Load += new System.EventHandler(this.frmEmpleados_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titulo;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Formularios/frmEmpleados.cs
+++ Formularios/frmEmpleados.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SistemaRestaurante.Controlador;
+using SistemaRestaurante.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,79 @@ namespace SistemaRestaurante.Formularios
         public frmEmpleados()
         {
             InitializeComponent();
+        }
+
+        public void ListarTodos()
+        {
+            dataGridView1.DataSource = null;
+
+            var lista = TLista.ListaMeseros
+                .Cast<object>()
+                .Concat(TLista.ListaCocineros.Cast<object>())
+                .ToList();
+
+            dataGridView1.DataSource = lista;
+        }
+
+        public void ListarMeseros()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = TLista.ListaMeseros.ToList();
+        }
+
+        public void ListarCocineros()
+        {
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = TLista.ListaCocineros.ToList();
+        }
+
+        private void frmEmpleados_Load(object sender, EventArgs e)
+        {
+            //comboBox1.Items.Add("Todos");
+            comboBox1.Items.Add("Mesero");
+            comboBox1.Items.Add("Cocinero");
+
+            comboBox1.SelectedIndex = 0;
+
+            //ListarTodos();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentRow != null)
+            {
+                object obj = dataGridView1.CurrentRow.DataBoundItem;
+
+                if (obj is Mesero m)
+                {
+                    MessageBox.Show(
+                        m.ObtenerInfo(),
+                        "Información Mesero");
+                }
+
+                if (obj is Cocinero c)
+                {
+                    MessageBox.Show(
+                        c.ObtenerInfo(),
+                        "Información Cocinero");
+                }
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "Todos")
+            {
+                //ListarTodos();
+            }
+            else if (comboBox1.SelectedItem.ToString() == "Mesero")
+            {
+                ListarMeseros();
+            }
+            else
+            {
+                ListarCocineros();
+            }
         }
     }
 }
